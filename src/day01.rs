b67fbcb252cea22c3c2dcc5a6d72e5ffb1c741b8
Oleg Rudenko <shturzys@gmail.com>
2022-06-26 15:49:31 -0700

    01

--- src/day01.rs
+++ src/day01.rs
@@ -1,16 +1,20 @@
+use crate::parsing::Gather;
+use std::iter::successors;
+
 pub fn run(input: &str) -> (i64, i64) {
-    _ = input;
-    (0, 0)
+    let fuel: Vec<i64> = input.lines().gather();
+
+    (fuel.iter().map(|&x| x / 3 - 2).sum(), fuel.iter().copied().map(rec_fuel).sum())
+}
+
+fn rec_fuel(x: i64) -> i64 {
+    successors(Some(x), |&y| Some(y / 3 - 2)).skip(1).take_while(|&y| y > 0).sum()
 }
 
 #[cfg(test)] 
 pub mod tests {
     #[test]
-    fn check_sample() {
-        assert_eq!(super::run(include_str!("../input/01-sample.txt")), (0, 0));
-    }
-    #[test]
     fn check_input() {
-        assert_eq!(super::run(include_str!("../input/01.txt")), (0, 0));
+        assert_eq!(super::run(include_str!("../input/01.txt")), (3_198_599, 4_795_042));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use advent2019::*;
 
 fn main() {
-    dbg!(day01::run(include_str!("../input/01-sample.txt")));
+    // dbg!(day01::run(include_str!("../input/01-sample.txt")));
     dbg!(day01::run(include_str!("../input/01.txt")));
 }
 
